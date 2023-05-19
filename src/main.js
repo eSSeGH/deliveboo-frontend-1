cc8ffa286916d8c87f77a525cb782cee9657697f
Bernardo Greco <Bernardo.greco98@gmail.com>
2023-05-19 16:53:08 +0200

    FEAT setup repo

---
+++ src/main.js
@@ -0,0 +1,27 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+// import { router } from './router'
+
+//FONTS
+import "@fontsource/kanit"
+import "@fontsource/red-hat-display"
+import "@fontsource/pt-sans"
+// /FONTS
+
+//FONTAWSOME
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faUserSecret)
+// /FONTAWESOME
+
+//se non funziona scambiare .component con .use (.use(router))
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
---
+++ src/router.js
@@ -0,0 +1,17 @@
+// import { createRouter, createWebHistory } from 'vue-router'; 
+// // import AppHome from './pages/AppHome.vue'; 
+// // import PostList from './pages/PostList.vue';
+
+// const router = createRouter({ 
+//     history: createWebHistory(),
+//     routes: [ 
+//     {
+//         // path: '/', name: 'home', component: AppHome
+//     }, 
+//     {
+//         // path: '/blog', name: 'posts', component: PostList
+//     }, 
+// ] 
+// });
+
+// export { router };
