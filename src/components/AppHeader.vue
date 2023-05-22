b0658f1b9a974f8d1027721246c7f7c96b93db8b
eSSeGH <ste.virgadaula@gmail.com>
2023-05-22 11:18:18 +0200

    SETUP components scaffoding

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,17 @@
+<template>
+    <header>
+
+    </header>
+</template>
+
+<script>
+export default {
+    setup() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped></style>
