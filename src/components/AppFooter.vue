916ebcbd3717abca29cdb59806617e30fcce0616
Bernardo Greco <Bernardo.greco98@gmail.com>
2023-05-22 17:28:13 +0200

    feat: footer ended add: waves images

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,8 +1,12 @@
 <template>
+    <div class="container-auto">
+        <img class="wave" src="/imgs/waves/footer-wave-desktop.svg" alt="">
+    </div>
+
     <div class="container-auto footer-bg">
 
         <div class="container-sm py-3 d-flex align-items-center justify-content-center">
-            <img class="logo-thumb" src="/imgs/logo/logo3.png" alt="">
+            <img class="logo-thumb" src="/imgs/logo/logo4.png" alt="">
         </div>
     
         <div class="container-sm py-3 d-flex flex-wrap justify-content-between pb-4 text-center text-white row-cols-1 row-cols-sm-2 row-cols-md-4">
@@ -27,10 +31,7 @@
 
             <div class="col d-flex flex-column pb-4 gap-2 my-footer-text">
                 <h3 class="my-footer-title">Follow us</h3>
-                <div class="col">Careers</div>
-                <div class="col">Careers</div>
-                <div class="col">Careers</div>
-                <div class="col">Careers</div>
+                <FollowButton class="follow-btn" />
             </div>
 
             <div class="col d-flex flex-column pb-4 gap-2 my-footer-text uppercase">
@@ -46,14 +47,21 @@
 </template>
 
 <script>
+import FollowButton from './elements/FollowButton.vue';
+
     export default {
-        
+        components: {
+            FollowButton,
+        }
     }
 </script>
 
 <style lang="scss" scoped>
 @use '../styles/main.scss' as *;
 @use '../styles/partials/colors.scss' as *;
+.wave {
+    width: 100%;
+}
 .footer-bg {
     background-color: $footer-bg;
 }
@@ -66,7 +74,17 @@
     cursor: pointer;
 }
 
+.my-footer-title {
+    margin-bottom: 1rem;
+}
+
 .uppercase {
     text-transform: uppercase;
+}
+
+.follow-btn {
+    display: flex;
+    align-items: center;
+    justify-content: center;
 }
 </style>
