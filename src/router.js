9f9f892fad57fce90544e2ab55b66a45f939bb98
Bernardo Greco <Bernardo.greco98@gmail.com>
2023-05-23 10:48:59 +0200

    feat: add router pages

--- src/router.js
+++ src/router.js
@@ -1,36 +1,36 @@
-// import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 
-// import Home from './pages/Home.vue'; 
-// import RestaurantsIndex from './pages/Restaurants.index.vue';
-// import RestaurantsShow from './pages/Restaurants.show.vue';
+import Home from './pages/Home.vue'; 
+import RestaurantsIndex from './pages/Restaurants.index.vue';
+import RestaurantsShow from './pages/Restaurants.show.vue';
 // import PageNotFound from './404.vue';
 
-// const router = createRouter({
-//      
-//     history: createWebHistory(),
+const router = createRouter({
+     
+    history: createWebHistory(),
 
-//     routes: [
-//     {
-//         path: '/',
-//         name: 'home',
-//         component: Home,
-//     }, 
-//     {
-//         path: '/restaurants',
-//         name: 'restaurants.index', 
-//         component: RestaurantsIndex,
-//     },
-//     {
-//         path: '/restaurants/:slug', //maybe slug is to insert here
-//         name: 'restaurants.show', 
-//         component: RestaurantsShow,
-//     },
-//     {
-//         path: '/404',
-//         name: '404',
-//         component: PageNotFound,
-//     }
-// ] 
-// });
+    routes: [
+    {
+        path: '/',
+        name: 'home',
+        component: Home,
+    }, 
+    {
+        path: '/restaurants',
+        name: 'restaurants.index', 
+        component: RestaurantsIndex,
+    },
+    {
+        path: '/restaurants/:slug', //maybe slug is to insert here
+        name: 'restaurants.show', 
+        component: RestaurantsShow,
+    },
+    // {
+    //     path: '/404',
+    //     name: '404',
+    //     component: PageNotFound,
+    // }
+] 
+});
 
-// export { router };
+export { router };
