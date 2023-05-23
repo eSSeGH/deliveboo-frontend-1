b469536120d7545d17ec73c67ebe7f80d77de3f1
eSSeGH <ste.virgadaula@gmail.com>
2023-05-23 17:06:13 +0200

    FEAT responsiveness added to header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,19 +3,19 @@
         <div class="container-sm">
             <div class="row justify-content-between align-items-center">
 
-                <div class="col-auto logo d-none d-md-block">
-                    <img src="/imgs/logo/logo3.png" alt="">
+                <div class="my-col logo d-none d-md-block">
+                    <img src="/imgs/logo/logo1.png" alt="">
                 </div>
-                <div class="col-auto logo-sm d-md-none">
+                <div class="my-col logo-sm d-md-none">
                     <img src="/imgs/logo/logo-sm.png" alt="">
                 </div>
 
-                <div class="col-auto searchbar-box">
+                <div class="my-col searchbar-box">
                     <input type="text" name="search" class="searchbar" placeholder="Cerca ristorante...">
                 </div>
 
-                <div class="header-buttons col-auto d-flex gap-4">
-                    <button class="login-btn">
+                <div class="header-buttons my-col d-flex justify-content-end">
+                    <button class="login-btn me-lg-4 me-md-3 me-sm-2 me-1">
                         <div class="sign"><svg viewBox="0 0 512 512">
                                 <path
                                     d="M217.9 105.9L340.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L217.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1L32 320c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM352 416l64 0c17.7 0 32-14.3 32-32l0-256c0-17.7-14.3-32-32-32l-64 0c-17.7 0-32-14.3-32-32s14.3-32 32-32l64 0c53 0 96 43 96 96l0 256c0 53-43 96-96 96l-64 0c-17.7 0-32-14.3-32-32s14.3-32 32-32z">
@@ -57,8 +57,6 @@ export default {
 .header-home {
     background-color: $orange-3;
     height: 90px;
-    z-index: -3;
-    position: relative;
 
     .container {
         height: 100%;
@@ -68,16 +66,16 @@ export default {
         margin: 0 !important;
         padding: 0 !important;
         height: 100%;
+        flex-wrap: nowrap;
 
         .col {
-            margin: 0 !important;
-            padding: 0 !important;
+            flex-basis: auto;
         }
 
         .logo {
             height: 80px;
             width: 311px;
-            flex-shrink: 0 !important;
+            flex-shrink: 0;
 
             img {
                 height: 100%;
@@ -87,14 +85,30 @@ export default {
         .logo-sm {
             height: 80px;
             aspect-ratio: 1;
+            flex-basis: 311px;
+            flex-shrink: 2;
+            min-width: 80px;
+            margin: 0 !important;
+            padding: 0 !important;
 
             img {
                 height: 100%;
             }
         }
 
+        .logo,
+        .logo-sm {
+
+            &:hover {
+                filter: drop-shadow(0 0 10px yellow);
+                transition: .3s;
+            }
+        }
+
         .searchbar-box {
-            flex-shrink: 10;
+            flex-shrink: 1;
+            display: inline;
+            max-width: 311px;
 
             .searchbar {
                 height: 24px;
@@ -105,7 +119,9 @@ export default {
                 cursor: pointer;
                 padding: 7px 12px;
                 transition: all 1s;
-                flex-shrink: 350px;
+                flex-shrink: 1;
+                width: 100%;
+
             }
         }
 
@@ -113,84 +129,89 @@ export default {
             outline-color: yellow;
         }
 
-        .login-btn,
-        .register-btn {
-            display: flex;
-            align-items: center;
-            justify-content: flex-start;
-            width: 45px;
-            height: 45px;
-            border: none;
-            border-radius: 50%;
-            cursor: pointer;
-            position: relative;
-            overflow: hidden;
-            transition-duration: .3s;
-            box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.199);
-            background-color: $purple-4;
+        .header-buttons {
+            flex-basis: 311px;
+            flex-shrink: 2;
 
-            &:hover {
-                width: 125px;
-                border-radius: 40px;
+            .login-btn,
+            .register-btn {
+                display: flex;
+                align-items: center;
+                justify-content: flex-start;
+                width: 45px;
+                height: 45px;
+                border: none;
+                border-radius: 50%;
+                cursor: pointer;
+                position: relative;
+                overflow: hidden;
+                transition-duration: .3s;
+                box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.199);
+                background-color: $purple-4;
+
+                &:hover {
+                    width: 125px;
+                    border-radius: 40px;
+                    transition-duration: .3s;
+                }
+
+                &:hover .sign {
+                    width: 30%;
+                    transition-duration: .3s;
+                    padding-left: 20px;
+                }
+
+                &:hover .log-text {
+                    opacity: 1;
+                    width: 70%;
+                    transition-duration: .3s;
+                    padding-right: 10px;
+                }
+
+                &:active {
+                    transform: translate(2px, 2px);
+                }
+            }
+
+            .sign {
+                width: 100%;
+                transition-duration: .3s;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+            }
+
+            .sign svg {
+                width: 17px;
+            }
+
+            .sign-register svg {
+                width: 17px;
+                transform: rotate(270deg);
+                position: relative;
+                left: 14px;
+            }
+
+            .sign svg path {
+                fill: white;
+            }
+
+            .sign-register svg path {
+                fill: white;
+            }
+
+
+
+            .log-text {
+                position: absolute;
+                right: 0%;
+                width: 0%;
+                opacity: 0;
+                color: white;
+                font-size: 1.2em;
+                font-weight: 600;
                 transition-duration: .3s;
             }
-
-            &:hover .sign {
-                width: 30%;
-                transition-duration: .3s;
-                padding-left: 20px;
-            }
-
-            &:hover .log-text {
-                opacity: 1;
-                width: 70%;
-                transition-duration: .3s;
-                padding-right: 10px;
-            }
-
-            &:active {
-                transform: translate(2px, 2px);
-            }
-        }
-
-        .sign {
-            width: 100%;
-            transition-duration: .3s;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-        }
-
-        .sign svg {
-            width: 17px;
-        }
-
-        .sign-register svg {
-            width: 17px;
-            transform: rotate(270deg);
-            position: relative;
-            left: 14px;
-        }
-
-        .sign svg path {
-            fill: white;
-        }
-
-        .sign-register svg path {
-            fill: white;
-        }
-
-
-
-        .log-text {
-            position: absolute;
-            right: 0%;
-            width: 0%;
-            opacity: 0;
-            color: white;
-            font-size: 1.2em;
-            font-weight: 600;
-            transition-duration: .3s;
         }
     }
 }
