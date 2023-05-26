8e727754eaecd88a199fc496f8d1413b7f10d48b
Bernardo Greco <Bernardo.greco98@gmail.com>
2023-05-26 23:31:34 +0200

    edit: category cards style added

--- src/components/Pages/Home.vue
+++ src/components/Pages/Home.vue
@@ -12,8 +12,8 @@
                         <iframe src="https://embed.lottiefiles.com/animation/4762"></iframe>
                     </div>
     
-                    <div class="col-6">
-                        <div class="d-flex flex-column gap-2">
+                    <div class="col col-md-6 text-center text-md-start">
+                        <div class="d-flex justify-content-center flex-column gap-2 px-3">
                             <h1 class="big-text">The Best Food Delivery</h1>
                             <p>Cibo Italiano, Giapponese, Cinese, Internazionale e molto altro</p>
                         </div>
@@ -36,11 +36,16 @@
 
     <section class="container-auto my-space">
 
-        <h2 class="text-center pb-5 mb-5">Ecco tutte le nostre categorie</h2>
+        <h2 class="text-center mb-5">Scegli tra le nostre categorie</h2>
 
-        <div class="d-flex gap-2 flex-wrap justify-content-center align-items-center">
-            <div class="card px-2" v-for="category in categories" :key="category.name">
-                {{ category }}
+        <div class="d-flex gap-3 flex-wrap justify-content-center align-items-center text-center">
+            <div class="my-card col-4 col-sm-3 col-md-2" v-for="category in categories" :key="category.name">
+                <div class="category-box">
+                    <img class="category-thumb" src="/public/imgs/temporary-img/giapponese.jpg" alt="">
+                    <div class="text-box">
+                        {{ category }}
+                    </div>
+                </div>
             </div>
         </div>
 
@@ -115,7 +120,31 @@ import axios from 'axios';
 .big-text {
     font-weight:bold !important;
 }
-.card {
+.my-card {
     cursor: pointer;
+    .category-box {
+        position: relative;
+        object-fit: cover;
+        object-position: center;
+        box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
+        border-radius: 10px;
+        .category-thumb {
+            border-radius: 10px;
+            filter: blur(1px);
+        }
+        .text-box {
+            background-color: rgba(0, 0, 0, 0.53);
+            position: absolute;
+            bottom: 0;
+            width: 100%;
+            color: white;
+            font-weight: bold;
+            border-radius: 0 0 10px 10px;
+        }
+    }
+    .category-thumb:hover {
+        filter: blur(0px);
+    }
+
 }
 </style>
